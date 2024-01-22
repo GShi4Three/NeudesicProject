87832eb90625ea198014a0403c5089ffcd7fde79
Gavin Shi <157427859+GShi4Three@users.noreply.github.com>
2024-01-22 18:00:13 -0500

    Outputting values
    This commit calculates a couple more key values and outputs them to the terminal.

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -16,11 +16,19 @@ public class MortgageCalculator {
 
         System.out.println("Input a value for the loan period in years: ");
         loanLengthYears = scanner.nextInt();
-        // Calculculating monthly mortgage payment
+        // Calculating monthly mortgage payment
         int loanLengthMonths = loanLengthYears * 12;
         double monthlyInterestRate = (annualInterestRate / 100) / 12;
         double monthlyMortgagePayment = principleAmount * (monthlyInterestRate * Math.pow((1 + monthlyInterestRate), loanLengthMonths)) / (Math.pow((1 + monthlyInterestRate), loanLengthMonths) - 1);
+        // Calculating other important static values to output in the terminal
+        double totalInterest = (monthlyMortgagePayment * loanLengthMonths) - principleAmount;
+        double totalPayment = principleAmount + totalInterest;
 
+        System.out.println("\nThe monthly mortgage payment is $" + Math.round(monthlyMortgagePayment * 100) / 100.0);
+        System.out.println("The number of monthly payments is " + loanLengthMonths);
+        System.out.println("The total interest amount is $" + Math.round(totalInterest * 100) / 100.0);
+        System.out.println("The total payment amount is $" + Math.round(totalPayment * 100) / 100.0);
+        
         scanner.close();
     }
 }
