5ceb586d1b6f031fba456359e9324dbbea1e4624
Gavin Shi <157427859+GShi4Three@users.noreply.github.com>
2024-01-22 18:10:59 -0500

    Calculating and outputting monthly values
    This commit calculates key monthly values and outputs them into the terminal.

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class MortgageCalculator {
 
@@ -29,6 +30,32 @@ public class MortgageCalculator {
         System.out.println("The total interest amount is $" + Math.round(totalInterest * 100) / 100.0);
         System.out.println("The total payment amount is $" + Math.round(totalPayment * 100) / 100.0);
         
+        double beginningBalance = principleAmount;
+        double cumulativePaymentAmount = 0;
+        double cumulativePrincipleAmount = 0;
+        double cumulativeInterestAmount = 0;
+        
+        for(int i = 1; i <= loanLengthMonths; i++){
+            double monthlyInterestAmmount = beginningBalance * monthlyInterestRate;
+            double monthlyPrincipleAmmount = monthlyMortgagePayment - monthlyInterestAmmount;
+            double remainingBalance = beginningBalance - (monthlyMortgagePayment - (beginningBalance * monthlyInterestRate));
+
+            cumulativePaymentAmount += monthlyMortgagePayment;
+            cumulativeInterestAmount += monthlyInterestAmmount;
+            cumulativePrincipleAmount += monthlyPrincipleAmmount;
+
+            System.out.println("Month " + i);
+            System.out.println("Beginning Balance: $" + Math.round(beginningBalance * 100) / 100.0);
+            System.out.println("Remaining Balance: $" + Math.round(remainingBalance * 100) / 100.0);
+            System.out.println("Payment Amount: $" + Math.round((monthlyMortgagePayment) * 100) / 100.0);
+            System.out.println("Cumulative Payment Amount: $" + Math.round((cumulativePaymentAmount) * 100) / 100.0);
+            System.out.println("Principle Amount: $" + Math.round((monthlyPrincipleAmmount) * 100) / 100.0);
+            System.out.println("Cumulative Principle Amount: $" + Math.round((cumulativePrincipleAmount) * 100) / 100.0);
+            System.out.println("Interest Amount: $" + Math.round((monthlyInterestAmmount) * 100) / 100.0);
+            System.out.println("Cumulative Interest Amount: $" + Math.round((cumulativeInterestAmount) * 100) / 100.0);
+           
+            beginningBalance = remainingBalance;
+        }
         scanner.close();
     }
 }
