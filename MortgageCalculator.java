a6b9711363737aa20530fb92a2d5469c6362b188
Gavin Shi <157427859+GShi4Three@users.noreply.github.com>
2024-01-22 18:17:47 -0500

    Adding comments and changing variable names
    Added comments and changed some variable names for clarity

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -24,28 +24,32 @@ public class MortgageCalculator {
         // Calculating other important static values to output in the terminal
         double totalInterest = (monthlyMortgagePayment * loanLengthMonths) - principleAmount;
         double totalPayment = principleAmount + totalInterest;
-
+        // Outputting values to terminal
         System.out.println("\nThe monthly mortgage payment is $" + Math.round(monthlyMortgagePayment * 100) / 100.0);
         System.out.println("The number of monthly payments is " + loanLengthMonths);
         System.out.println("The total interest amount is $" + Math.round(totalInterest * 100) / 100.0);
         System.out.println("The total payment amount is $" + Math.round(totalPayment * 100) / 100.0);
-        
-        double beginningBalance = principleAmount;
+        /*  
+        * Inititializing variables that will increment/decrement as the for loop runs. They must be initialized outside the loop
+        * because their current values are needed to calculate their future values
+        */
+        double startingBalance = principleAmount;
         double cumulativePaymentAmount = 0;
         double cumulativePrincipleAmount = 0;
         double cumulativeInterestAmount = 0;
         
         for(int i = 1; i <= loanLengthMonths; i++){
-            double monthlyInterestAmmount = beginningBalance * monthlyInterestRate;
+            // Initializing values that change each month
+            double monthlyInterestAmmount = startingBalance * monthlyInterestRate;
             double monthlyPrincipleAmmount = monthlyMortgagePayment - monthlyInterestAmmount;
-            double remainingBalance = beginningBalance - (monthlyMortgagePayment - (beginningBalance * monthlyInterestRate));
-
+            double remainingBalance = startingBalance - (monthlyMortgagePayment - (startingBalance * monthlyInterestRate));
+            // Incrementing cumulative amounts
             cumulativePaymentAmount += monthlyMortgagePayment;
             cumulativeInterestAmount += monthlyInterestAmmount;
             cumulativePrincipleAmount += monthlyPrincipleAmmount;
-
+            // Outputting values to terminal
             System.out.println("Month " + i);
-            System.out.println("Beginning Balance: $" + Math.round(beginningBalance * 100) / 100.0);
+            System.out.println("Starting Balance: $" + Math.round(startingBalance * 100) / 100.0);
             System.out.println("Remaining Balance: $" + Math.round(remainingBalance * 100) / 100.0);
             System.out.println("Payment Amount: $" + Math.round((monthlyMortgagePayment) * 100) / 100.0);
             System.out.println("Cumulative Payment Amount: $" + Math.round((cumulativePaymentAmount) * 100) / 100.0);
@@ -53,8 +57,8 @@ public class MortgageCalculator {
             System.out.println("Cumulative Principle Amount: $" + Math.round((cumulativePrincipleAmount) * 100) / 100.0);
             System.out.println("Interest Amount: $" + Math.round((monthlyInterestAmmount) * 100) / 100.0);
             System.out.println("Cumulative Interest Amount: $" + Math.round((cumulativeInterestAmount) * 100) / 100.0);
-           
-            beginningBalance = remainingBalance;
+            // Setting next month's starting balance to the current month's remaining balance
+            startingBalance = remainingBalance;
         }
         scanner.close();
     }
