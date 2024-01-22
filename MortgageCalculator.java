b9c994e6c24f0e150f3c89e59963c839696c5047
Gavin Shi <157427859+GShi4Three@users.noreply.github.com>
2024-01-22 17:54:47 -0500

    Prompting user for values
    This change imports the java.util.Scanner library and prompts users for 3 values that are necessary to calculate the monthly mortgage payment amount.

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class MortgageCalculator {
 
     public static void main (String[] args){
-        
+         Scanner scanner = new Scanner(System.in);
+
+        double principleAmount, annualInterestRate;
+        int loanLengthYears;
+        // Prompting users for necessary values to calculate the monthly mortgage payment
+        System.out.println("Input a value for the principle loan amount: ");
+        principleAmount = scanner.nextDouble();
+
+        System.out.println("Input a percentage for the annual interest rate: ");
+        annualInterestRate = scanner.nextDouble();
+
+        System.out.println("Input a value for the loan period in years: ");
+        loanLengthYears = scanner.nextInt();
+
+        scanner.close();
     }
 }
