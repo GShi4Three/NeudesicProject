1641e995a85620c3e4915c00346b1a1aab26d62f
Gavin Shi <157427859+GShi4Three@users.noreply.github.com>
2024-01-22 19:09:36 -0500

    Creating new function for formatting purposes
    Created a function named storingData() to clean up the main function and compartmentalize the code.

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -6,17 +6,50 @@ import javax.swing.JTable;
 
 public class MortgageCalculator {
 
+    private static String[][] storingData(double principleAmount, double monthlyMortgagePayment, double monthlyInterestRate, int loanLengthMonths) {
+        /*  
+        * Inititializing variables that will increment/decrement as the for loop runs. They must be initialized outside the loop
+        * because their current values are needed to calculate their future values
+        */
+        double startingBalance = principleAmount;
+        double cumulativePaymentAmount = 0;
+        double cumulativePrincipleAmount = 0;
+        double cumulativeInterestAmount = 0;
+        // Initializing a 2D string array to store the calculated values into the table
+        String[][] data = new String[loanLengthMonths][9];
+        
+        for(int i = 1; i <= loanLengthMonths; i++){
+            // Initializing values that change each month
+            double monthlyInterestAmmount = startingBalance * monthlyInterestRate;
+            double monthlyPrincipleAmmount = monthlyMortgagePayment - monthlyInterestAmmount;
+            double remainingBalance = startingBalance - (monthlyMortgagePayment - (startingBalance * monthlyInterestRate));
+            // Incrementing cumulative values
+            cumulativePaymentAmount += monthlyMortgagePayment;
+            cumulativeInterestAmount += monthlyInterestAmmount;
+            cumulativePrincipleAmount += monthlyPrincipleAmmount;
+            // Storing appropriate values in each cell of the table
+            data[i - 1][0] = "Month " + i;
+            data[i - 1][1] = "$" + Math.round(startingBalance * 100) / 100.0;
+            data[i - 1][2] = "$" + Math.round(remainingBalance * 100) / 100.0;
+            data[i - 1][3] = "$" + Math.round((monthlyMortgagePayment) * 100) / 100.0;
+            data[i - 1][4] = "$" + Math.round((cumulativePaymentAmount) * 100) / 100.0;
+            data[i - 1][5] = "$" + Math.round((monthlyPrincipleAmmount) * 100) / 100.0;
+            data[i - 1][6] = "$" + Math.round((cumulativePrincipleAmount) * 100) / 100.0;
+            data[i - 1][7] = "$" + Math.round((monthlyInterestAmmount) * 100) / 100.0;
+            data[i - 1][8] = "$" + Math.round((cumulativeInterestAmount) * 100) / 100.0;
+            // Setting next month's starting balance to the current month's remaining balance
+            startingBalance = remainingBalance;
+        }
+        return data;
+    }
     private static void amortizationScheduleTable(String[][] data){
         // Initiallizing frame
         JFrame f = new JFrame("Amortization Schedule Calculator");
-        
         // Creating column names
-        String[] columnNames = { "Payment Month", "Starting Balance", "Remaining Balance", "Payment Amount", "Total Balance Paid",
+        String[] columnNames = { "Payment Month", "Starting Balance", "Remaining Balance", "Payment Amount", "Total Amount Paid",
         "Principle Amount", "Total Principle Paid",  "Interest Amount", "Total Interest Paid" };
- 
         // Initializing table
         JTable t = new JTable(data, columnNames);
-
         // Initialling scroll pane
         JScrollPane sp = new JScrollPane(t);
         // Adding scroll pane to frame
@@ -54,39 +87,8 @@ public class MortgageCalculator {
         System.out.println("The number of monthly payments is " + loanLengthMonths);
         System.out.println("The total interest amount is $" + Math.round(totalInterest * 100) / 100.0);
         System.out.println("The total payment amount is $" + Math.round(totalPayment * 100) / 100.0);
-        /*  
-        * Inititializing variables that will increment/decrement as the for loop runs. They must be initialized outside the loop
-        * because their current values are needed to calculate their future values
-        */
-        double startingBalance = principleAmount;
-        double cumulativePaymentAmount = 0;
-        double cumulativePrincipleAmount = 0;
-        double cumulativeInterestAmount = 0;
-        // Initializing a 2D string array to store the calculated values into the table
-        String[][] data = new String[loanLengthMonths][9];
-        
-        for(int i = 1; i <= loanLengthMonths; i++){
-            // Initializing values that change each month
-            double monthlyInterestAmmount = startingBalance * monthlyInterestRate;
-            double monthlyPrincipleAmmount = monthlyMortgagePayment - monthlyInterestAmmount;
-            double remainingBalance = startingBalance - (monthlyMortgagePayment - (startingBalance * monthlyInterestRate));
-            // Incrementing cumulative amounts
-            cumulativePaymentAmount += monthlyMortgagePayment;
-            cumulativeInterestAmount += monthlyInterestAmmount;
-            cumulativePrincipleAmount += monthlyPrincipleAmmount;
-            // Storing appropriate values in each cell of the table
-            data[i - 1][0] = "Month " + i;
-            data[i - 1][1] = "$" + Math.round(startingBalance * 100) / 100.0;
-            data[i - 1][2] = "$" + Math.round(remainingBalance * 100) / 100.0;
-            data[i - 1][3] = "$" + Math.round((monthlyMortgagePayment) * 100) / 100.0;
-            data[i - 1][4] = "$" + Math.round((cumulativePaymentAmount) * 100) / 100.0;
-            data[i - 1][5] = "$" + Math.round((monthlyPrincipleAmmount) * 100) / 100.0;
-            data[i - 1][6] = "$" + Math.round((cumulativePrincipleAmount) * 100) / 100.0;
-            data[i - 1][7] = "$" + Math.round((monthlyInterestAmmount) * 100) / 100.0;
-            data[i - 1][8] = "$" + Math.round((cumulativeInterestAmount) * 100) / 100.0;
-            // Setting next month's starting balance to the current month's remaining balance
-            startingBalance = remainingBalance;
-        }
+        // Storing calculated monthly values into a 2D array to display in the table
+        String[][] data = storingData(principleAmount, monthlyMortgagePayment, monthlyInterestRate, loanLengthMonths);
         // Opening the window and displaying the table
         amortizationScheduleTable(data);
 
